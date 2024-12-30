bfaf4d6a39684532280336c50309a649fe028747
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 16:32:30 -0500

    usando el objeto Model

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
@@ -1,9 +1,11 @@
 package com.jhonatan.spring.boot.wed.app.controllers;
 
+import com.jhonatan.spring.boot.wed.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/app")
@@ -13,5 +15,17 @@ public class IndexController {
     public String index(Model modelo) {
         modelo.addAttribute("titulo", "Hola Spring con Model.");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrez");
+        usuario.setApellido("Peralta");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil de Usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
     }
 }
