15469c0d1698a7da3dfe793c6d9d93b3f38a2fc6
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 14:40:42 -0500

    configuracion del proyecto

---
+++ src/main/java/com/jhonatan/spring/boot/wed/app/SpringbootWedAppApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatan.spring.boot.wed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonatan/spring/boot/wed/app/SpringbootWedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.spring.boot.wed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
