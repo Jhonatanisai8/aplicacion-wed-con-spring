a9d9ce6620aad89f6233eb918a34d3218984e82e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 16:55:46 -0500

    directiva if de Thymeleaf

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package com.jhonatan.spring.boot.wed.app.controllers;
 
 import com.jhonatan.spring.boot.wed.app.models.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +24,19 @@ public class IndexController {
         Usuario usuario = new Usuario();
         usuario.setNombre("Andrez");
         usuario.setApellido("Peralta");
-
+        //usuario.setEmail("andrez@gmail.com.pe");
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Perfil de Usuario: ".concat(usuario.getNombre()));
 
         return "perfil";
     }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", "Listado de Usuarios.");
+        List<Usuario> usuarios = new ArrayList<>();
+        model.addAttribute("usuarios", usuarios);
+        return "listar";
+    }
+
 }
--- src/main/java/com/jhonatan/spring/boot/wed/app/models/Usuario.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/models/Usuario.java
@@ -4,6 +4,7 @@ public class Usuario {
 
     private String nombre;
     private String apellido;
+    private String email;
 
     public Usuario() {
     }
@@ -11,6 +12,14 @@ public class Usuario {
     public Usuario(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getNombre() {
