d75577192e6bd135abe98583b8b04353cf438f7a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 15:59:44 -0500

    anotacion @RequestMapping sobre el controlador

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
@@ -3,8 +3,10 @@ package com.jhonatan.spring.boot.wed.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping("/index")
