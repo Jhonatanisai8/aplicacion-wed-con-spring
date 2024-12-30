360fc2df77c80e27d407b9a2e7924e91d38c4cfa
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 15:32:41 -0500

    pasar datos a la vista forma 04 con Model

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
@@ -1,17 +1,15 @@
 package com.jhonatan.spring.boot.wed.app.controllers;
 
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String index(Map<String, String> model) {
-        model.put("titulo", "Hola Spring Framework 02. con ");
+    public String index(Model modelo) {
+        modelo.addAttribute("titulo", "Hola Spring con Model.");
         return "index";
     }
 }
