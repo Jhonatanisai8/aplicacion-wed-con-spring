b1f2d5913777ee8f077858b431cf738a47693bbe
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-01 11:30:38 -0500

    uso de la anotacion @Model-Attribute

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,15 +35,17 @@ public class IndexController {
     @RequestMapping("/listar")
     public String listar(Model model) {
         model.addAttribute("titulo", "Listado de Usuarios.");
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios") //se utiliza para vincular datos al modelo y hacer que estos datos estén disponibles en las vistas 
+    public List<Usuario> poblarUsuarios() {
         List<Usuario> usuarios = Arrays.asList(
                 new Usuario("David", "Calle", "davidcalle34@gmail.com"),
                 new Usuario("Juan", "Perez", "juanperex231@gmail.com"),
                 new Usuario("Daniel", "Castillo", "danielcastillo908@gmail.com"),
                 new Usuario("Damasco", "Rosado", "damasco909@gmail.com")
         );
-
-        model.addAttribute("usuarios", usuarios);
-        return "listar";
+        return usuarios;
     }
-
 }
