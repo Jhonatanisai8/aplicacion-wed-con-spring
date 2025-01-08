96ec4dc6d69d0f689f6009e9e9ed804ec73d544e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-08 18:16:04 -0500

    obtener varias parametros de la URL con @REQUESTPARAM forma 02

--- src/main/java/com/jhonatan/spring/boot/wed/app/controllers/EjemploParamsController.java
+++ src/main/java/com/jhonatan/spring/boot/wed/app/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.jhonatan.spring.boot.wed.app.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +29,23 @@ public class EjemploParamsController {
     public String param(@RequestParam String saludo,
             @RequestParam Integer numero,
             Model modelo) {
-        modelo.addAttribute("resultado", "El saludo enviado es: '"+saludo + "' y el numero es: '"+numero+"'");
+        modelo.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el numero es: '" + numero + "'");
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request,
+            Model modelo) {
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+            numero = Integer.valueOf(request.getParameter("numero"));
+
+        } catch (NumberFormatException e) {
+            numero = 0;
+        }
+
+        modelo.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el numero es: '" + numero + "'");
         return "params/ver";
     }
 
